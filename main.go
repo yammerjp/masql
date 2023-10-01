bc223eaf64acd2fc73471de36a7f4940a87f708a
Keisuke Nakayama <me@yammer.jp>
2023-10-01 18:36:11 +0900

    wip

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+  "reflect"
 
 	"github.com/pingcap/tidb/parser"
 	"github.com/pingcap/tidb/parser/ast"
-	_ "github.com/pingcap/tidb/parser/test_driver"
+	"github.com/pingcap/tidb/parser/test_driver"
 )
 
 func parse(sql string) (*ast.StmtNode, error) {
@@ -20,10 +21,48 @@ func parse(sql string) (*ast.StmtNode, error) {
 }
 
 func main() {
-	astNode, err := parse("SELECT a, b FROM t")
+	astNode, err := parse("INSERT INTO customers (id, name) VALUES (1, \"John\");")
 	if err != nil {
 		fmt.Printf("parse error: %v\n", err.Error())
 		return
 	}
-	fmt.Printf("%v\n", *astNode)
+	fmt.Printf("%v\n", extract(astNode))
+}
+
+type colX struct{
+	colNames []string
+  tableName string
+  columnNameListOpt []string
+  valueSym string
+  valuesList []string
+  // TableName
+  // ColumnNameListOpt
+  // ValueSym
+  // ValuesList
+}
+
+func (v *colX) Enter(in ast.Node) (ast.Node, bool) {
+  switch node := in.(type) {
+  case *ast.ColumnName:
+		v.colNames = append(v.colNames, node.Name.O)
+  case *ast.TableName:
+    v.tableName = node.Name.O;
+  case *test_driver.ValueExpr:
+    fmt.Printf("type: %v\n", node.Type)
+    v.valuesList = append(v.valuesList, node.GetString())
+  }
+
+  fmt.Printf("type! %v\n", reflect.TypeOf(in))
+
+	return in, false
+}
+
+func (v *colX) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
+
+func extract(rootNode *ast.StmtNode) colX {
+	v := &colX{}
+	(*rootNode).Accept(v)
+	return *v
 }
